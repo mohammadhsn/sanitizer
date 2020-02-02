a921ab471e38a2f8e2391112e2afbd109552892c
Mohammad <hosseini.mohamad94@gmail.com>
2020-02-02 14:48:05 +0330

    Wrap input string to class

---
+++ src/Input.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App;
+
+use App\Sanitize\SanitizerInterface;
+
+class Input
+{
+    protected $stream;
+
+    /**
+     * @var SanitizerInterface []
+     */
+    protected $sanitizers = [];
+
+    public function __construct(string $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    public function addSanitizer(SanitizerInterface $sanitizer): self
+    {
+        $this->sanitizers [] = $sanitizer;
+
+        return $this;
+    }
+
+    protected function clean(): self
+    {
+        foreach ($this->sanitizers as $sanitizer) {
+            $this->stream = $sanitizer->sanitize($this->stream);
+        }
+
+        return $this;
+    }
+
+    public function getValue(): string
+    {
+        return $this->clean()->stream;
+    }
+}
---
+++ tests/InputTest.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use App\Input;
+use App\Sanitize\SanitizerInterface;
+use PHPUnit\Framework\TestCase;
+
+class InputTest extends TestCase
+{
+    protected $input;
+
+    protected function setUp(): void
+    {
+        $this->input = new Input('some content');
+    }
+
+    public function test_it_applies_sanitizers()
+    {
+        $html = Mockery::mock(SanitizerInterface::class);
+        $html->shouldReceive('sanitize')
+            ->andReturn('hello');
+
+
+        $this->input->addSanitizer($html);
+
+        $this->assertEquals('hello', $this->input->getValue());
+
+        $special = Mockery::mock(SanitizerInterface::class);
+        $special->shouldReceive('sanitize')
+            ->andReturn('acme');
+
+        $this->input->addSanitizer($special);
+
+        $this->assertEquals('acme', $this->input->getValue());
+    }
+}
